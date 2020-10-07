7ddaf598f49594cc52d1b68b5dfe4da873915f6b
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-10-07 13:46:15 +0300

    Adding Favicons

--- head.php
+++ head.php
@@ -6,7 +6,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?=$title?> | Soon Developments Ltd - Clearing, Deliveries, and Logistics Experts Worldwide </title>
-    <link rel="icon" href="img/favicon.png">
+    
+    <!-- Icons  -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- animate CSS -->
