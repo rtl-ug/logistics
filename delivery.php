d4e82cae31891bf8517dbade2025acc271743710
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-10-07 10:00:02 +0300

    make index php too

---
+++ delivery.php
@@ -0,0 +1,136 @@
+<?php
+    $title='About';
+    include 'head.php'?>
+    <!-- breadcrumb start-->
+    <section class="breadcrumb breadcrumb_bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner text-center">
+                        <div class="breadcrumb_iner_item">
+                            <h2>Deliveries</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb start-->
+
+    <!--::resent deliveries start::-->
+    <section class="deliveries_part padding_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="deliveries_tittle">
+                        <h2>Recent Deliveries</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="deliveries_slider owl-carousel">
+                        <div class="single_deliveries">
+                            <img src="img/deliveries/deliveries_1.png" alt="">
+                            <div class="single_deliveries_text">
+                                <h3>World Corgo</h3>
+                                <div class="single_deliveries_text_iner">
+                                    <img src="img/deliveries_img.png" alt="">
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_deliveries">
+                            <img src="img/deliveries/deliveries_2.png" alt="">
+                            <div class="single_deliveries_text">
+                                <h3>World Corgo</h3>
+                                <div class="single_deliveries_text_iner">
+                                    <img src="img/deliveries_img.png" alt="">
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_deliveries">
+                            <img src="img/deliveries/deliveries_3.png" alt="">
+                            <div class="single_deliveries_text">
+                                <h3>World Corgo</h3>
+                                <div class="single_deliveries_text_iner">
+                                    <img src="img/deliveries_img.png" alt="">
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_deliveries">
+                            <img src="img/deliveries/deliveries_1.png" alt="">
+                            <div class="single_deliveries_text">
+                                <h3>World Corgo</h3>
+                                <div class="single_deliveries_text_iner">
+                                    <img src="img/deliveries_img.png" alt="">
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                    <p>324 King Heaven tower, House no
+                                        Melbourne ,VIC-222, Australia</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--::resent deliveries end::-->
+
+    <!-- deliverie_process part start-->
+    <section class="deliverie_process section_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7 col-sm-10">
+                    <div class="section_tittle">
+                        <h2>World Wide <br>
+                            <span>Deliveries Process</span>
+                        </h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_deliverie_process">
+                        <img src="img/icon/process_1.svg" alt="">
+                        <h4>Trucks 50%</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_deliverie_process">
+                        <img src="img/icon/process_2.svg" alt="">
+                        <h4>Air 10%</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_deliverie_process">
+                        <img src="img/icon/process_3.svg" alt="">
+                        <h4>Railway 20%</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_deliverie_process">
+                        <img src="img/icon/process_4.svg" alt="">
+                        <h4>Ship 20%</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- deliverie_process part end-->
+    <?php include 'footer.php'?>
